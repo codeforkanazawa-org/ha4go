289b1b154ffe9dbaab9487419d20b09ffd3acb00
Junya Shimazu <shimazu-junya@kayac.com>
2015-04-26 22:29:53 +0900

    add migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426080713) do
+ActiveRecord::Schema.define(version: 20150426132854) do
 
   create_table "project_updates", force: :cascade do |t|
     t.integer  "project_id"
@@ -47,5 +47,7 @@ ActiveRecord::Schema.define(version: 20150426080713) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
