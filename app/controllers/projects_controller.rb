dce26c58237fe014c4c6816c5759cc05ff88c6f0
Junya Shimazu <shimazu-junya@kayac.com>
2015-06-07 12:02:15 +0900

    fix top

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,11 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
+  # GET /projects
+  def index
+    @projects = Project.all
+  end
+
   # GET /projects/1
   def show
   end
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,8 @@
 class TopController < ApplicationController
+  LIMIT_ROWS = 10 # プロジェクト表示件数
+
   def index
-    @projects = Project.all
+    @projects = Project.last(LIMIT_ROWS).reverse
   end
 
   def help
