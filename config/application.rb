7decaffed4d5d03dd4c5c83214622f2bd7ad720d
PharaohKJ <kato@phalanxware.com>
2015-08-10 13:19:41 +0900

    ディレクトリ構造の整理と設定の反映。 top/indexはいったんi18n化した。

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Ha4go
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
 
     I18n.available_locales = [:en, :ja]
