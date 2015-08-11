3fb2dc0ab29450d686968fbbb537ef33bc4ba40e
Yukimitsu Izawa <izawa@hd012108.clwit.co.jp>
2015-08-11 11:30:24 +0900

    modify TZ to JST+9

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module Ha4go
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
