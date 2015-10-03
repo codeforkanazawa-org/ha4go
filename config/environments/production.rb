68c964851c342bf3b9f4457253c9dffc04443cef
PharaohKJ <kato@phalanxware.com>
2015-10-03 16:42:08 +0900

    support cfk smtp.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,15 +65,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address:               Rails.application.secrets.smtp_address,
-      port:                  Rails.application.secrets.smtp_port,
-      domain:                Rails.application.secrets.smtp_domain,
-      user_name:             Rails.application.secrets.smtp_user_name,
-      password:              Rails.application.secrets.smtp_password,
-      authentication:        'plain',
-      enable_starttls_auto:  true
+    address:               Rails.application.secrets.smtp_address,
+    port:                  Rails.application.secrets.smtp_port,
+    domain:                Rails.application.secrets.smtp_domain,
+    user_name:             Rails.application.secrets.smtp_user_name,
+    password:              Rails.application.secrets.smtp_password,
+    authentication:        :plain,
+    enable_starttls_auto:  true,
+    openssl_verify_mode:   'none'
   }
-
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
