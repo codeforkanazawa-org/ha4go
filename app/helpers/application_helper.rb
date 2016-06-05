c58424d36211f49715ef03d2cd501f6caf3b4ba4
PharaohKJ <kato@phalanxware.com>
2016-06-06 03:44:42 +0900

    Support nil description of user.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,7 @@ module ApplicationHelper
   require "uri"
 
   def description_text(text)
+    text = text.to_s
     URI.extract(text, ['http', 'https']).uniq.each do |url|
       sub_text = ""
       sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
