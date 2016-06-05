a6b8216f54c287a24724a9f6942f56a31560a213
PharaohKJ <kato@phalanxware.com>
2016-06-06 02:44:08 +0900

    Support #68. Support #96. Support #90. (maybe)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,17 @@ module ApplicationHelper
     return text if text.nil?
     text = h text
     text.gsub(/\r\n|\r|\n/, '<br/>').html_safe
+
+  require "uri"
+
+  def description_text(text)
+    URI.extract(text, ['http', 'https']).uniq.each do |url|
+      sub_text = ""
+      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+
+      text.gsub!(url, sub_text)
+    end
+    text.gsub!(/\r\n|\r|\n/, '<br/>')
+    text.html_safe
   end
 end
