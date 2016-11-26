c2ddffed59afe328c3f50f8c60dedc24c9eef58d
kimihito <tatsurotamashiro@gmail.com>
2016-11-26 22:39:26 +0900

    projectのデータを追加

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -20,4 +20,16 @@ load_seed_from_yaml('users.yml') do |user|
   user = User.from_omniauth(auth)
   user.update(email: auth.email, description: "私の名前は #{user.name}")
 end
-load_seed_from_yaml('projects.yml')
+
+load_seed_from_yaml('projects.yml') do |project|
+  params = JSON.parse(project.to_json, object_class: OpenStruct)
+  project = Project.new(
+    subject: params.subject,
+    description: params.description,
+    stage_id: params.stage_id,
+    user_id: params.user_id
+  )
+  project.skills << Skill.all
+  project.users << User.find(params.user_id)
+  project.save
+end
