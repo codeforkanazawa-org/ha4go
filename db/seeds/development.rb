94d1094351847975ca8ef97341a81499bcdf03e8
kimihito <tatsurotamashiro@gmail.com>
2016-11-26 20:47:18 +0900

    指定した初期データ用のYAMLファイルがない場合はスキップする

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -6,11 +6,18 @@ def read_relative(path)
   File.read relative_path path
 end
 
-YAML.load(read_relative('users.yml')).each do |user|
+def load_seed_from_yaml(file_name)
+  file_path = relative_path(file_name)
+  return warn "#{file_name} does not exist file." unless File.exist?(file_path)
+  if block_given?
+    yml = YAML.load(read_relative(file_name))
+    yml.each { |y| yield y }
+  end
+end
+
+load_seed_from_yaml('users.yml') do |user|
   auth = JSON.parse(user.to_json, object_class: OpenStruct)
   user = User.from_omniauth(auth)
-  user.update({
-    email: auth.email,
-    description: "私の名前は #{user.name}"
-  })
+  user.update(email: auth.email, description: "私の名前は #{user.name}")
 end
+load_seed_from_yaml('projects.yml')
