7d0ffddba3563d0644d024277bc917fc087a9f74
kimihito <tatsurotamashiro@gmail.com>
2016-12-04 08:20:04 +0900

    存在しないアクションのテストを削除

--- test/controllers/project_updates_controller_test.rb
+++ test/controllers/project_updates_controller_test.rb
@@ -6,12 +6,6 @@ class ProjectUpdatesControllerTest < ActionController::TestCase
     @controller.session[:user_id] = @project_update.user.id
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:project_updates)
-  end
-
   test "should get new" do
     get :new
     assert_response :success
