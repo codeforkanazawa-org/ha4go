292a69639b5fbff432360441d355ea99aeeca048
PharaohKJ <kato@phalanxware.com>
2017-02-11 19:30:35 +0900

    FBの顔画像を渡しても表示できないようなのでやめた。

--- app/controllers/concerns/sns_publisher.rb
+++ app/controllers/concerns/sns_publisher.rb
@@ -16,15 +16,14 @@ module SnsPublisher
 
   # localhost:3000 などでデバッグしていると必ず失敗するので留意すること
   # (FBが localhost へのリンクを投稿しようとすると必ず失敗するようになっているようだ)
-  def publish_to_sns_page(text, uri, name, description, image)
+  def publish_to_sns_page(text, uri, name, description, _image)
     return if ENV['FACEBOOK_PAGE_SECRET_TOKEN'].nil?
     fb_page = Koala::Facebook::API.new(ENV['FACEBOOK_PAGE_SECRET_TOKEN'])
     # params are to see http://www.rubydoc.info/github/arsduo/koala/Koala%2FFacebook%2FGraphAPIMethods%3Aput_wall_post
     post_params = {
       link:        absolute_url(uri),
       name:        name,
-      description: description,
-      picture:     image
+      description: description
     }
     post_params.each do |k, v|
       logger.debug("[PUT_WALL_POST] #{k} : #{v}")
