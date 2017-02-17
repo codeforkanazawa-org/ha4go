d23c83b7d6fb772d101d85831584d98fd8baeb69
PharaohKJ <kato@phalanxware.com>
2017-02-17 19:32:04 +0900

    ha4go から Ha4go へ。

--- app/helpers/seo_helper.rb
+++ app/helpers/seo_helper.rb
@@ -1,9 +1,10 @@
+# coding: utf-8
 module SeoHelper
   def default_meta_tags
     {
       og: {
-        title: 'ha4go',
-        site_name: 'ha4go',
+        title: 'Ha4go',
+        site_name: 'Ha4go',
         type: 'article',
         url: request.url,
         description: '課題を深く知る市民とシビックテックの情報交換サイト、はじめました。',
