a0ff928b0a1b8f205f8defd22d71de9a4a3c37e4
kimihito <tatsurotamashiro@gmail.com>
2017-08-06 20:18:49 +0900

    Returns files erased erroneously

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
+Rails.application.config.assets.paths << "#{Rails}/vendor/assets/fonts"
 Rails.application.config.assets.precompile += %w( jquery.js bootstrap.js jquery_ujs.js matsu/*)
